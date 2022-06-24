bbea864a7ee0adb3255498754d215dc668329a32
Rumata-SL <kirr7619@gmail.com>
2022-06-25 01:12:41 +0500

    added text to login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -6,6 +6,7 @@ export const Login = () => {
         <div className={s.root}>
             <h1>Login</h1>
             <p>This is login</p>
+            <p>This is login 2</p>
         </div>
     );
 };
